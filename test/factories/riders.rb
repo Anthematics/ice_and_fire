862d9c947e49ed67e1fa507d18cb306f8934a3eb
Anthematics <jasonharder2@gmail.com>
2017-09-19 18:02:09 -0400

    everything works cept email does not pass uniqueness

--- test/factories/riders.rb
+++ test/factories/riders.rb
@@ -1,4 +1,6 @@
 FactoryGirl.define do
   factory :rider do
+    nickname 'Peanut Butter & Jam'
+    email "@westeros.com"
   end
 end
--- test/models/rider_test.rb
+++ test/models/rider_test.rb
@@ -5,6 +5,7 @@ class RiderTest < ActiveSupport::TestCase
   def test_that_riders_swing
     # Use your knowledge of factories to make the test pass!
     # this time use create( ... ) instead of build
+    #at first i got an error because i typed create(name:) ignoring the fact i had to specify it was a rider i was creating.
     aegon = create(:rider, name:"Aegon", nickname:"Agon", sword: 'Blackfyre')
     daenerys = create(:rider, name:"Daenerys", nickname: "danys", sword: 'Lightbringer')
 
